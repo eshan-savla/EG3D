79b366dc88d5fdf3745786e75372f154e907f22c
Eshan <eshan.savla@gmail.com>
2023-02-20 00:01:57 +0100

    misc changes

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, const char * argv[]) {
     raw_input.ReadCloud("../data/Blech.pcd");
 //    raw_input.GenerateCloud(1000);
     raw_input.VoxelDownSample(0.0001f);
-    raw_input.StatOutlierRemoval(50,1.0);
+//    raw_input.StatOutlierRemoval(10,1.0);
     std::cout << "Points after down sample: " << raw_input.GetCount() << std::endl;
     std::vector<int> edge_points;
     pcl::StopWatch stpw;
@@ -22,18 +22,19 @@ int main(int argc, const char * argv[]) {
     stpw.reset();
     pcl::PointCloud<pcl::PointXYZ>::Ptr edge_cloud (new pcl::PointCloud<pcl::PointXYZ>);
     *edge_cloud = raw_input.FindEdgePoints(200, M_PI_2, 0.01);
-    EdgeCloud edges(edge_cloud);
+    EdgeCloud edges;
+    edges.AddPoints(edge_cloud);
     double duration = stpw.getTimeSeconds();
+    edges.StatOutlierRemoval(10, 1.0);
     std::cout << "Processing duration: " << duration << std::endl;
     edges.SaveCloud("../data/edge_points.pcd");
     std::cout << "Segmenting edges" << std::endl;
 //    EdgeCloud edges;
 //    edges.ReadCloud("../data/edge_points.pcd");
     stpw.reset();
-    edges.SegmentEdges(15, 0.01, 10.0 / 180.0 * M_PI, true, false);
+    edges.SegmentEdges(20, 0.01, 11.0 / 180.0 * M_PI, true, false);
     duration = stpw.getTimeSeconds();
     std::cout << "Segmenting duration: " << duration << std::endl;
     edges.CreateColouredCloud("../data/segments.pcd");
-    //TODO:Consider visualisation implementation
     return 0;
 }
